95207ba84d4201957b75d161995f62db1d3a96d8
ericruizmolero <154838631+ericruizmolero@users.noreply.github.com>
2024-02-26 17:51:10 +0100

    Slots

--- src/index.ts
+++ src/index.ts
@@ -15,17 +15,26 @@ window.Webflow.push(() => {
   const calendar = new Calendar(calendarElement, {
     plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
     initialView: 'dayGridMonth',
-    locale: esLocale,
+    // locale: esLocale,
     headerToolbar: {
       left: 'prev,next today',
       center: 'title',
       right: 'dayGridMonth,timeGridWeek,listWeek',
     },
     events,
+    dayCellClassNames: function (dateInfo) {
+      const dateStr = dateInfo.date.toISOString().split('T')[0];
+      if (!activeDates.has(dateStr)) {
+        return ['no-event-day']; // Return a class for days without events
+      }
+      return []; // Return an empty array for days with events
+    },
   });
 
   calendar.render();
 });
+
+const activeDates = new Set();
 
 const getEvents = (): Event[] => {
   const scripts = document.querySelectorAll<HTMLScriptElement>('[data-element="event-data"]');
@@ -33,8 +42,19 @@ const getEvents = (): Event[] => {
   const events = [...scripts].map((script) => {
     const event: Event = JSON.parse(script.textContent!);
     event.start = new Date(event.start);
-    event.end = new Date(event.end);
+    const endDate = new Date(event.end);
+    // Adjust the end date for internal calculation but not for the activeDates inclusion
+    event.end = new Date(endDate.setDate(endDate.getDate() + 1));
     event.title = htmlDecode(decodeURIComponent(event.title));
+
+    // Populate activeDates for all days from start to end of the event, excluding the artificially added last day
+    const currentDate = new Date(event.start);
+    while (currentDate < event.end) {
+      // Use < instead of <= to exclude the extra day
+      activeDates.add(currentDate.toISOString().split('T')[0]);
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+
     return event;
   });
 
--- src/types.ts
+++ src/types.ts
@@ -2,4 +2,5 @@ export type Event = {
   title: string;
   start: string | Date;
   end: string | Date;
+  color?: string; // Optional property to specify the event's background color
 };
